174f5c37321755200ff52fe147a885793ea7ce74
Johnatan <john.acdc@hotmail.com>
2019-06-10 20:33:53 -0300

    - Rotação da pedra.

--- Assets/Scripts/StoneController.cs
+++ Assets/Scripts/StoneController.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class StoneController : MonoBehaviour {
 
     public float velocidade;
+
+    public float rotacao;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
         transform.position = new Vector3(transform.position.x + velocidade, transform.position.y, transform.position.z);
+        rb.rotation -= rotacao;
     }
 }
