ba9f530218ff84cc4efbe1adda3e89feba0d96d1
Johnatan <john.acdc@hotmail.com>
2019-06-10 21:18:39 -0300

    - Criado inimigo

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    private Animator anim;
+
+    private float posInicial;
+
+    public float velocidade;
+
+    public float tempoAnim;
+
+    private float lastAnim;
+
+    // Start is called before the first frame update
+    void Start() {
+        anim = GetComponent<Animator>();
+        posInicial = transform.position.x;
+        lastAnim = Time.time;
+        anim.SetBool("Andando", false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        print(anim.GetBool("Andando"));
+        if (anim.GetBool("Andando") && Time.time - lastAnim <= tempoAnim) {
+            float novoX = transform.position.x + (velocidade * Time.deltaTime);
+            transform.position = new Vector3(novoX, transform.position.y, transform.position.z);
+        } else if (Time.time - lastAnim > tempoAnim) {
+            anim.SetBool("Andando", !anim.GetBool("Andando"));
+            lastAnim = Time.time;
+            if (anim.GetBool("Andando")) {
+                float escalaX = -transform.localScale.x;
+                velocidade = -velocidade;
+                transform.localScale = new Vector3(escalaX, transform.localScale.y, transform.localScale.z);
+            }
+        }
+        
+    }
+}
